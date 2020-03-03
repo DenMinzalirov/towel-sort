5e2371e0bae612c4ca741e8b23cad3fbc4c51789
Den Minzakirov <Den.Minzakirov@gmail.com>
2020-03-03 14:58:31 +0300

    feat: complite Towel Sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    let result = []
+    if (matrix === undefined) {
+        return result;
+    }
+    for (let i = 0; i < matrix.length; i++) {
+        if ((i + 1) % 2 === 0) {
+            matrix[i].reverse();
+        }       
+        result = result.concat(matrix[i])
+    }
+  return result;
 }
